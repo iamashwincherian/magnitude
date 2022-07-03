39f0a5d0dc706dea15fe45fe822b7892dbe7e9c4
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2022-07-03 09:13:56 +0530

    init commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Index() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+}
